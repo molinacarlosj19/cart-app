b6267ca334fe351149626c636669d2083708ac8c
Carlos Molina <molinacarlosj@gmail.com>
2023-08-14 23:05:44 -0400

    Create CartApp Component, add Bootstrap , products and productService

---
+++ src/data/products.js
@@ -0,0 +1,44 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecanico',
+        description: 'Teclado Mecanico con luces RGB',
+        price: 1000,
+
+    },
+    {
+        id: 2,
+        name: 'Samsung Smart TV 55',
+        description: 'Excelente TV',
+        price: 3000,
+
+    },
+    {
+        id: 3,
+        name: 'Audifono Bluetooh Sony',
+        description: 'Audifono Bluetooh Sony para Smartphone',
+        price: 770,
+
+    },
+    {
+        id: 4,
+        name: 'Memoria Corsair 8GB',
+        description: 'Memoria Corsair 8GB Optimizada para Juego',
+        price: 3700,
+
+    },
+    {
+        id: 5,
+        name: 'Asus NVidia RTX',
+        description: 'Asus NVidia RTX para juegos en 4K',
+        price: 5000,
+
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core I5',
+        description: 'CPU Intel Core I5 para tareas Multicore',
+        price: 4000,
+
+    }
+]
